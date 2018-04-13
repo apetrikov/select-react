e6aa4eea24a54c81eb1690bea62234e221a5e0cf
Andrey Petrikov <apetrikov@rtlservice.com>
2018-04-13 11:25:20 +0300

    Выпадающему списку присваивается класс, в зависимости от нужного направления раскрытия

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -2,37 +2,77 @@ import React from "react";
 
 import style from './select.styl';
 
-const Select = ({ orderedList = [], search = '', onInput}) => {
+class Select extends React.Component{
+  constructor(props){
+    super(props);
 
-  const preparedList = orderedList.map((name, i) => {
-    const highlight = (str, length) => (<span className="highlight">{str.slice(0, length)}</span>);
-      return (
-        <li key={i} onClick={() => onInput(name)}>
-          <a>{highlight(name, search.length)}{name.slice(search.length)}</a>
-        </li>)
+    this.selectInput = React.createRef();
+
+    this.state = {
+      toTop: 0,
+      toBottom: 0,
     }
-  );
+  }
 
-  const dropdown = orderedList.length
-    ? (<ul className="sub-menu">
-         {preparedList}
-       </ul>)
-    : null;
+  componentDidMount() {
+    this.updateWindowHeight();
+  };
 
-  return (
-    <div className="select">
-      <input
-        type="text"
-        className="input"
-        value={search}
-        pattern=".*"
-        maxLength="16"
-        required
-        onInput={(e) => onInput(e.target.value)}/>
-      <span className="floating-label">Выберите страну</span>
-      {dropdown}
-    </div>
-  );
-};
+  updateWindowHeight() {
+    const getFreeSpace = () => {
+      const { top, bottom } = this.selectInput.current.getBoundingClientRect();
+      const height = window.innerHeight;
+      return {
+        toTop: top,
+        toBottom: height - bottom
+      }
+    };
+    this.setState(getFreeSpace());
+  };
+
+  chooseDirection(length) {
+    const { toTop, toBottom } = this.state;
+    const submenuHeight = length * 50; // фиксированная высота 50 px
+    if (submenuHeight <= toBottom) return 'down';
+    if (submenuHeight <= toTop) return 'up';
+    return 'down';
+  };
+
+  render() {
+    const { orderedList = [], search = '', onInput} = this.props;
+
+    const preparedList = orderedList.map((name, i) => {
+        const highlight = (str, length) => (<span className="highlight">{str.slice(0, length)}</span>);
+        return (
+          <li key={i} onClick={() => onInput(name)}>
+            <a>{highlight(name, search.length)}{name.slice(search.length)}</a>
+          </li>)
+      }
+    );
+
+    const dropdown = orderedList.length
+      ? (<ul className={`sub-menu ${this.chooseDirection(orderedList.length)}`} ref={this.subMenu}>
+        {preparedList}
+      </ul>)
+      : null;
+
+    return (
+      <div className="select">
+        <input
+          type="text"
+          className="input"
+          value={search}
+          pattern=".*"
+          maxLength="16"
+          required
+          ref={this.selectInput}
+          onInput={(e) => onInput(e.target.value)}/>
+        <span className="floating-label">Выберите страну</span>
+        {dropdown}
+      </div>
+    )
+  }
+}
+
 
 export default Select;
